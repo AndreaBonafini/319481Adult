2780f251a25dcbcc7e907b59ad5f5ad49afdcf4d
AndreaBonafini <47528585+AndreaBonafini@users.noreply.github.com>
2019-03-05 15:14:54 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _319481Adult2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnQuestion_Click(object sender, RoutedEventArgs e)
+        {
+            
+            int aAge;
+            int.TryParse(txtAge.Text, out aAge);
+            string output = "";
+        
+            if (aAge >= 16)
+            {
+                output = "You are allowed to drive";
+            }
+            if (aAge >= 18)
+            {
+                output +=",vote";
+            }
+            if (aAge >= 19)
+            {
+                output += " and drink";
+            }
+            MessageBox.Show(output);
+        }    
+    }
+}
